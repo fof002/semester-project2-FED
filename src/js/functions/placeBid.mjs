6f896c8ee27fd521f73a48d0be656baa4b37085f
fof002 <80104002+fof002@users.noreply.github.com>
2023-05-03 15:00:58 +0200

    small changes

--- src/js/functions/placeBid.mjs
+++ src/js/functions/placeBid.mjs
@@ -3,7 +3,9 @@ import { url } from "../BASE_URL.mjs";
 const placeBidUrl = url + "id" + "/bids";
 
 export function openBidModal(event) {
-  const bidItemId = getAttribute("data-item-id");
-  const newBidUrl = "index.html?" + bidItemId;
-  window.history.replaceState("state", "title", newBidUrl);
+  if (event.target.matches(".place-bid-btn")) {
+    const bidItemId = event.target.getAttribute("data-item-id");
+    const newBidUrl = "index.html?biddingOn=" + bidItemId;
+    window.history.replaceState("state", "title", newBidUrl);
+  }
 }
